f526761942ea7a7cc80379bef2cd439345a52302
Gnanathilagar <108898617+Gnanathilagar@users.noreply.github.com>
2022-09-25 10:26:41 -0700

    1.tabcontroler created 2.chat screen work started

---
+++ lib/chat_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  Color greenColorButton = const Color(0xFF26d367);
+  Color greenColor = const Color(0xFF008071);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.message,
+        ),
+        backgroundColor: greenColor,
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80"),
+            ),
+            title: Text("Murugesan Tendulkar"),
+            subtitle: Text("Hi"),
+
+            trailing: Text("12.00"),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled4/chat_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -8,20 +9,57 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  List<Tab> tabs = <Tab>[
+    Tab(icon: Icon(Icons.camera_alt),),
+    Tab(
+      text: 'CHATS',
+
+    ),
+    Tab(text: 'STATUS'),
+    Tab(
+      text: 'CALLS',
+    ),
+
+  ];
   Color greenColor = const Color(0xFF007d6f);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: greenColor,
-        automaticallyImplyLeading: false,
-        title: const Text("WhatsApp"),
-        actions: [
-          Icon(Icons.search),
-          SizedBox(width: 16,),
-          Icon(Icons.more_vert),
-          SizedBox(width: 8,),
-        ],
+
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            labelStyle: TextStyle(fontSize: 16,fontWeight:FontWeight.bold),
+            labelColor: Colors.white,
+            indicatorColor: Colors.white,
+            tabs: tabs,
+          ),
+          backgroundColor: greenColor,
+          automaticallyImplyLeading: false,
+          title: const Text("WhatsApp"),
+          actions: const [
+            Icon(Icons.search),
+            SizedBox(
+              width: 16,
+            ),
+            Icon(Icons.more_vert),
+            SizedBox(
+              width: 8,
+            ),
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            ChatScreen(),
+            ChatScreen(),
+            Text('euwyriruiuig'),
+            Text('euwyriruiuig'),
+
+
+
+          ],
+        ),
       ),
     );
   }
